b164024ac9ed014b1d9683b6b6efeab07eb30a8a
gaurangp22 <thegaurangpant22@gmail.com>
2025-08-19 17:38:50 +0530

    new ones

--- app/committee/members/page.js
+++ app/committee/members/page.js
@@ -130,7 +130,7 @@ export default function TeamPage() {
       "Dr. Liudong Xing",
       "Ayman EL-Refaie",
       "Dr. Diane Rover",
-      "Dr. Diana Andone",
+      
     ],
 
     "National Advisory Committee": [
